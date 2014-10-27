2144ae58846e10dd3d45cbfc427ac73541afa043
ashtindell <tindell.ea@gmail.com>
2014-10-27 16:29:50 -0400

    finished user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  match '/signup',    to: 'users#new',      via: 'get'
+
+  resources :users, only:   [:create, :show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
