a7580ebb1c5558845d64137bc9d1c7d80ec67b71
ashtindell <tindell.ea@gmail.com>
2014-10-27 18:11:57 -0400

    added navigation

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   match '/about',   to: 'static_pages#about', via: 'get'
   match '/help',    to: 'static_pages#help',  via: 'get'
   match '/signup',  to: 'users#new',          via: 'get'
+  match '/signin',  to: 'sessions#new',       via: 'get'
+  match '/signout', to: 'sessions#destroy',   via: 'delete'
 
   resources :users, only:   [:create, :show]
 
