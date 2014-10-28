b4e42384763ad7afe887e2ef1597b219d8780bc4
ashtindell <tindell.ea@gmail.com>
2014-10-27 21:40:30 -0400

    finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id)
-      # sign_in @user
-      # redirect_back_or user_path(@user.id)
+      sign_in @user
+      redirect_back_or user_path(@user.id)
     else
       render 'new'
     end
