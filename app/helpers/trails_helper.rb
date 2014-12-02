6f4b3da3ee8e502fe766dc7dd2845656255bdfe7
ashtindell <tindell.ea@gmail.com>
2014-12-01 23:04:27 -0500

    refactor trail index and add styling

--- app/helpers/trails_helper.rb
+++ app/helpers/trails_helper.rb
@@ -2,4 +2,15 @@ module TrailsHelper
   def city(place)
     place["city"]
   end
+
+  def activity_selector
+    select_tag :activity_type_name, options_for_select(activity_options)
+  end
+
+  def activity_options
+    [ ["Choose One",""], ["Hiking","hiking"], ["Mountain biking","mountain biking"],
+      ["Campgrounds","campgrounds"], ["Trail Running","trail running"], 
+      ["Skiing / Snowboarding","snow sports"], ["Caving","caving"], 
+      ["Horseback Riding","horseback riding"] ]
+  end
 end
--- spec/controllers/folios_controller_spec.rb
+++ spec/controllers/folios_controller_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe FoliosController, :type => :controller do
   before do
     User.destroy_all
+    sign_in user, no_capybara: true
   end
 
   let(:user) { FactoryGirl.create(:user) }
-  before { sign_in user, no_capybara: true }
 
   describe "POST create folio" do
     context "valid attributes" do
@@ -67,80 +67,79 @@ describe FoliosController, :type => :controller do
     end
   end
 
-  # describe "GET index" do
+  describe "GET index" do
+    before { Folio.destroy_all }
 
-  #   # let(:folio) { FactoryGirl.create(:folio) }
+    let(:first_folio)  { FactoryGirl.create(:folio, title: "Favorites", user: user) }
+    let(:second_folio) { FactoryGirl.create(:folio, title: "Next Trip", user: user) }
 
-  #   let(:first_folio)  { FactoryGirl.create(:folio) }
-  #   let(:second_folio) { FactoryGirl.create(:folio, title: "Next Trip") }
+    it "renders :index" do
+      get :index
+      expect(response).to render_template(:index)
+    end
 
-  #   it "renders :index" do
-  #     get :index
-  #     expect(response).to render_template(:index)
-  #   end
+    it "assigns all folios to @folios as an array" do
+      get :index
+      expect(assigns(:folios)).to eq([first_folio, second_folio])
+    end
+  end
 
-  #   it "assigns all folios to @folios as an array" do
-  #     get :index
-  #     expect(assigns(:folios)).to eq([first_folio, second_folio])
-  #   end
-  # end
+  describe "GET edit folio" do
+    let(:folio) { FactoryGirl.create(:folio) }
+    it "renders :edit" do
+      get :edit, id: folio.id
+      expect(response).to render_template(:edit)
+    end
 
-#   describe "GET edit folio" do
-#     let(:folio) { FactoryGirl.create(:folio) }
-#     it "renders :edit" do
-#       get :edit, id: folio.id
-#       expect(response).to render_template(:edit)
-#     end
+    it "assigns requested folio to @folio" do
+      get :edit, id: folio.id
+      expect(assigns(:folio)).to eq(folio)
+    end
+  end
 
-#     it "assigns requested folio to @folio" do
-#       get :edit, id: folio.id
-#       expect(assigns(:folio)).to eq(folio)
-#     end
-#   end
+  describe "PUT update" do
+    let(:folio) { FactoryGirl.create(:folio) }
 
-#   describe "PUT update" do
-#     let(:folio) { FactoryGirl.create(:folio) }
+    context "valid attributes" do
+      it "changes @folio attributes" do
+        put :update, id: folio.id, folio: FactoryGirl.attributes_for(:folio, title: "Do the dishes")
+        folio.reload
+        expect(folio.title).to eq("Do the dishes")
+      end
 
-#     context "valid attributes" do
-#       it "changes @folio attributes" do
-#         put :update, id: folio.id, folio: FactoryGirl.attributes_for(:folio, title: "Do the dishes")
-#         folio.reload
-#         expect(folio.title).to eq("Do the dishes")
-#       end
+      it "redirects to :show" do
+        put :update, id: folio.id, folio: FactoryGirl.attributes_for(:folio, title: "Do the dishes")
+        last_folio = Folio.last
+        expect(response).to redirect_to(folio_path(last_folio.id))
+      end
+    end
 
-#       it "redirects to :show" do
-#         put :update, id: folio.id, folio: FactoryGirl.attributes_for(:folio, title: "Do the dishes")
-#         last_folio = Folio.last
-#         expect(response).to redirect_to(folio_path(last_folio.id))
-#       end
-#     end
+    context "invalid attributes" do
+      it "does not change @folio's attributes" do
+        put :update, id: folio.id, folio: FactoryGirl.attributes_for(:folio, title: " ")
+        folio.reload
+        expect(folio.title).to eq("Favorites")
+      end
 
-#     context "invalid attributes" do
-#       it "does not change @folio's attributes" do
-#         put :update, id: folio.id, folio: FactoryGirl.attributes_for(:folio, title: " ")
-#         folio.reload
-#         expect(folio.title).to eq("Favorites")
-#       end
+      it "re-renders :edit" do
+        put :update, id: folio.id, folio: FactoryGirl.attributes_for(:folio, title: " ")
+        expect(response).to render_template(:edit)
+      end
+    end    
+  end
 
-#       it "re-renders :edit" do
-#         put :update, id: folio.id, folio: FactoryGirl.attributes_for(:folio, title: " ")
-#         expect(response).to render_template(:edit)
-#       end
-#     end    
-#   end
+  describe "DELETE destroy" do
+    let!(:folio) { FactoryGirl.create(:folio) }
 
-#   describe "DELETE destroy" do
-#     let!(:folio) { FactoryGirl.create(:folio) }
+    it "deletes the folio" do
+      expect{
+        delete :destroy, id: folio.id
+      }.to change(Folio, :count).by(-1)
+    end
 
-#     it "deletes the folio" do
-#       expect{
-#         delete :destroy, id: folio.id
-#       }.to change(Folio, :count).by(-1)
-#     end
-
-#     it "redirects to :index" do
-#       delete :destroy, id: folio.id
-#       expect(response).to redirect_to(folios_path)
-#     end 
-#   end
+    it "redirects to :index" do
+      delete :destroy, id: folio.id
+      expect(response).to redirect_to(folios_path)
+    end 
+  end
 end
