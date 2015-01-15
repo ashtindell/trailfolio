0e6b3205ae91356b5331d4bbb5b5553e7a832b14
ashtindell <tindell.ea@gmail.com>
2015-01-14 21:51:39 -0500

    add development page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   match '/home',        to: 'static_pages#home',      via: 'get'
   match '/about',       to: 'static_pages#about',     via: 'get'
-  match '/help',        to: 'static_pages#help',      via: 'get'
+  match '/development', to: 'static_pages#development',      via: 'get'
 
   match '/signup',      to: 'users#new',              via: 'get'
   match '/signin',      to: 'sessions#new',           via: 'get'
